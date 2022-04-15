6ad36dc7a1b942ca74e1e825dab08678ccaac3ea
anggadsatria <angga.dsatria27@gmail.com>
2022-04-15 17:27:42 +0700

    add series sequlize models

---
+++ server/services/series/models/series.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Series extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Series.init({
+    name: DataTypes.STRING,
+    duration: DataTypes.INTEGER,
+    episodes: DataTypes.INTEGER,
+    seasons: DataTypes.INTEGER,
+    genre: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Series',
+  });
+  return Series;
+};

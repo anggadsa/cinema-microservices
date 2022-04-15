ab0ba2d01c5fd35c285170615f344f7f278fd212
anggadsatria <angga.dsatria27@gmail.com>
2022-04-15 19:03:26 +0700

    change get-all-series

--- server/services/series/app.js
+++ server/services/series/app.js
@@ -1,14 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 5000; 
 const { Series } = require(`./models`)
 
 app.use(express.json());
 app.use(express.urlencoded( {extended: true} ))
 
 app.get(`/series`, async (req, res) => {
-    
+    const options = {
+        attributes: {
+            exclude: ['createdAt', 'updatedAt']
+        }
+    }
+    try{
+        const findAll = await Series.findAll(options);
+        res.status(200).json({
+            status: 'Success',
+            result: findAll
+        })
+    } catch(err) {
+        console.log(err)
+    }
 });
 
 app.get('/series/:id', async (req,res) => {
